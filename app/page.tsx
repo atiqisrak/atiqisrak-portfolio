dd82161bd5c63b1671063d1fd7885be46a561830
atiqisrak <atiq.israk@webable.digital>
2025-08-23 21:58:57 +0600

    fixed left panel scroll

--- app/page.tsx
+++ app/page.tsx
@@ -32,12 +32,12 @@ export default function Home() {
   return (
     <div className="relative min-h-screen bg-background">
       <div ref={auraRef} className="mouse-aura" />
-      <div className="mx-auto max-w-screen-xl px-4 py-8 md:px-8 md:py-12 lg:px-12 lg:py-16 xl:px-24 xl:py-20">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 md:px-8 md:py-12 lg:px-12 lg:py-16 xl:px-24 lg:pt-0 xl:pt-0">
         <div className="lg:flex lg:justify-between lg:gap-8 xl:gap-12">
-          <div className="lg:w-1/2 lg:sticky lg:top-0 lg:h-screen lg:overflow-y-auto lg:pr-4 xl:pr-8">
+          <div className="lg:w-1/2 lg:fixed lg:top-0 lg:h-screen lg:pr-4 xl:pr-8 lg:pt-0">
             <Nav />
           </div>
-          <main className="lg:w-1/2 flex flex-col pt-6 lg:pt-24 gap-6 lg:gap-8">
+          <main className="lg:w-1/2 lg:ml-[50%] flex flex-col pt-6 lg:pt-24 gap-6 lg:gap-8">
             <About />
             <ExpCard />
             <Projects />
